9f2aba2a5d53a431ff3e52801940bafec6185678
TimurMP <titimurmamyshev@gmail.commur>
2022-06-30 20:16:09 +0300

    Interface Album added

--- src/telran/album/interfaces/Album.java
+++ src/telran/album/interfaces/Album.java
@@ -1,4 +1,17 @@
 package telran.album.interfaces;
 
+import telran.album.model.Photo;
+
+import java.time.LocalDate;
+
 public interface Album {
+    boolean addPhoto (Photo photo);
+    boolean removePhoto(int photoId, int albumId);
+    boolean updatePhoto (int photoID, int albumId, String url);
+    Photo getPhotoFromAlbum (int photoId, int albumId);
+    Photo[] getAllPhotosFromAlbum(int albumId);
+    Photo[] getPhotoBetweenDate(LocalDate dateFrom, LocalDate dateTo);
+    int size();
+
+
 }
