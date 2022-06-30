bc13c5d6bc2364f6ea7338195c14c5a7ffd0418e
TimurMP <titimurmamyshev@gmail.commur>
2022-07-01 00:50:24 +0300

    Baseline 1.6 @BeforeEach added

--- src/telran/album/tests/AlbumImplTest.java
+++ src/telran/album/tests/AlbumImplTest.java
@@ -2,13 +2,21 @@ package telran.album.tests;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import telran.album.dao.AlbumImpl;
+import telran.album.interfaces.Album;
+import telran.album.model.Photo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlbumImplTest {
+    Album album;
+    Photo[] myVacation;
 
     @BeforeEach
     void setUp() {
+        album = new AlbumImpl(5);
+        myVacation = new  Photo[3];
+        myVacation[0] = new Photo(0504, 001, "Sunset", "http://127.0.0.1/001", "01.01.2020");
     }
 
     @Test
